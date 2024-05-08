a816207d66a2a9308c5990a7f4ab91792f0fccc5
rodgerswisdom <trulyhawona@gmail.com>
2024-05-09 01:53:52 +0300

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Manifest to create a file in tmp
 
-file{ '/tmp/school':
-	ensure => file,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => "I love Puppet\n",
-
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }

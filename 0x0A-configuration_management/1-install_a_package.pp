af4f4e8cfc8fe525b997377aa8e3b6a402c6ff17
rodgerswisdom <trulyhawona@gmail.com>
2024-05-09 09:40:25 +0300

    Install Werkzeug 2.1.1 before flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# install flask from pip3.
+# Puppet manifest to install Flask version 2.1.0 with specific Werkzeug version from pip3
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug'],  # Ensure Werkzeug is installed before Flask
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.1',  # Specify the version of Werkzeug required by Flask
+  provider => 'pip3',
+}
+

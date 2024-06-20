0477d5722bd83ef7d7587ee962bfde67844f6d6a
rodgerswisdom <trulyhawona@gmail.com>
2024-06-20 10:37:07 +0300

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, returns 0.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Python:subreddit.subscriber.counter:v1.0 (by /u/yourusername)'}
-    
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
+        if response.status_code != 200:
             return 0
+        results = response.json().get("data", {})
+        return results.get("subscribers", 0)
     except requests.RequestException:
         return 0
 
